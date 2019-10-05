330cf538ce473ea92fdd5bdd2b475b58fa5cfe10
Ihar <petrushen@yahoo.com>
2019-10-05 21:34:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 module.exports = function zeros(expression) {
-   var number_arr = expression.split('*');
-// получается массив строк вида '8!' и '12!!'
+
   var count = 0;
+
+  var number_arr = expression.split('*');
+// получается массив строк вида '8!' и '12!!'
 
   for(let i = 0; i < number_arr.length; i++) {
 
     if(number_arr[i][number_arr[i].length - 1] == '!' &&
     number_arr[i][number_arr[i].length - 2] != '!') {
       let number = +number_arr[i].replace('!', '');
-  for(let k = 2; k <= number; k++) {
+  for(let k = 1; k <= number; k++) {
     let j = k;
     while(j%5 == 0) {
       j = j/5;
       count++;
     }
   }
-}
-if(number_arr[i][number_arr[i].length - 1] == '!' &&
-number_arr[i][number_arr[i].length - 2] == '!') {
-  let number = +number_arr[i].replace('!', '');
-    for(let k = number; k > 0; k-=2) {
-      let j = k;
+
+} else {
+  let number = +number_arr[i].replace(/!/g, '');
+  for(let k = number; k > 0; k-=2) {
+
+      if((k > 4) && (k%2 == 1) && (k%5 == 0)) {
+          count++;
+      }
+
+
+     
+      if(k%10 == 0 && number%2 == 0) {
+        count++;
+         let j = k/5;
       while(j%5 == 0) {
         j = j/5;
         count++;
       }
-    }
-
+      }
   }
+
+}
 
 }
 
